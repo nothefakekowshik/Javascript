17b06a3cd64aa443c5d7b3fb8e91de27b35d04a5
notthefakekowshik <kowsik02@gmail.com>
2022-10-03 18:50:08 +0530

    notes about hositing

--- TDZ.js
+++ TDZ.js
@@ -6,3 +6,4 @@ let a=10;
 var b=20;
 
 //the time at which the variable gets memory but not initialized with a value .that time is known as TDZ.
+//not only the var, even the let and const are hosted. but they will be in TDZ, thats why the error comes if we apply hoisting to let,const.
