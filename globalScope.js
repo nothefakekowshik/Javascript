57478bf0988d143c9c831b2b13281c2dca53cc35
notthefakekowsik <kowsik02@gmail.com>
2022-11-07 09:14:00 +0530

    default

---
+++ globalScope.js
@@ -0,0 +1,7 @@
+let b;
+{
+    var a = 10;
+    let b = 20;
+}
+console.log(a);
+console.log(b);
--- shadowing.js
+++ shadowing.js
@@ -4,3 +4,31 @@ var b=100;
     console.log(b);
 }
 console.log(b);
+
+//but this is not allowed.
+// let b=100
+// {
+//     var b = "hello";
+//     console.log(b);
+// }
+// console.log(b);
+
+console.log();
+var a = 100;
+{
+    var a = 10; // same name as global var
+    let b = 20;
+    const c = 30;
+    console.log(a); // 10
+    console.log(b); // 20
+    console.log(c); // 30 
+}
+console.log(a);
+
+
+//this is illegal shadowing.
+// let a = 20;
+// {
+//     var a = 20;
+// }
+// Uncaught SyntaxError: Identifier 'a' has already been declared
